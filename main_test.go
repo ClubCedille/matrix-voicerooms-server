0e428b58746a48b63780f1ac954ce79c39235002
Michael-John Sakellaropoulos <mjsakellaropoulos@gmail.com>
2022-06-04 16:23:32 -0400

    Initial attempt to get test framework running

---
+++ main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func SetUpRouter() *gin.Engine {
+	router := gin.Default()
+	return router
+}
+
+func TestPingRoute(t *testing.T) {
+	router := SetUpRouter()
+
+	w := httptest.NewRecorder()
+	req, _ := http.NewRequest(http.MethodGet, "/health", nil)
+	router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, "pong", w.Body.String())
+}
